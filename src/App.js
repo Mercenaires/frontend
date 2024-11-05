658fe429eac28a46bf2e44dbc0d90f90ca2f560c
JeffBalberan <40013016@parisnanterre.fr>
2024-11-05 19:51:55 +0100

    Ajout d'une nouvelle page contenant les jeux selon le MBTI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import SearchPage from './components/SearchPage';
-
+import CatMBTIGamePage from './components/CatMBTIGamePage';
 import Navbar from './components/Navbar';
 import MBTIPage from './components/MBTIPage';
 
@@ -15,6 +15,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/mbti" element={<MBTIPage />} />
+          <Route path="/catmbtipage" element={<CatMBTIGamePage />} />
       </Routes>
     </Router>
   );
